b994410b67670a29174bda84a1263dd47840d613
jimen <jimen@pcjimena.lan>
2024-02-22 21:23:09 +0100

    iñigo el de ampuero

---
+++ src/main/java/com/mycompany/roma/Roma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.roma;
+
+/**
+ *
+ * @author jimen
+ */
+public class Roma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
